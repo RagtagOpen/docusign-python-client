f7650585de58d9091532335b3b7df6b4125db866
dryan <dryan@users.noreply.github.com>
2019-06-03 10:15:21 -0400

    making requirements not specific versioned pinned

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 """
     DocuSign REST API
 
-    The DocuSign REST API provides you with a powerful, convenient, and simple Web services API for interacting with DocuSign.
+    The DocuSign REST API provides you with a powerful, convenient, and simple
+    Web services API for interacting with DocuSign.
 
     OpenAPI spec version: v2
     Contact: devcenter@docusign.com
@@ -11,9 +12,10 @@
 """
 
 
-import sys
 import os
-from setuptools import setup, find_packages, Command
+import sys
+
+from setuptools import Command, find_packages, setup
 
 NAME = "docusign_esign"
 VERSION = "2.0.0"
@@ -25,17 +27,32 @@ VERSION = "2.0.0"
 # http://pypi.python.org/pypi/setuptools
 
 
-REQUIRES = ["urllib3 >= 1.15", "six == 1.8.0", "certifi >= 14.05.14", "python-dateutil >= 2.5.3", "setuptools >= 21.0.0", "PyJWT==1.7.1", "cryptography==2.5", "nose==1.3.7"]
+REQUIRES = [
+    "urllib3 >= 1.15",
+    "six >= 1.8.0",
+    "certifi >= 14.05.14",
+    "python-dateutil >= 2.5.3",
+    "setuptools >= 21.0.0",
+    "PyJWT>=1.7.1",
+    "cryptography>=2.5",
+    "nose>=1.3.7",
+]
+
 
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
+
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
-        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
+
 
 setup(
     name=NAME,
@@ -47,10 +64,9 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    cmdclass={
-        'clean': CleanCommand,
-    },
-    long_description="""\
-    The DocuSign REST API provides you with a powerful, convenient, and simple Web services API for interacting with DocuSign.
-    """
+    cmdclass={"clean": CleanCommand},
+    long_description=(
+        "The DocuSign REST API provides you with a powerful, convenient, and "
+        "simple Web services API for interacting with DocuSign."
+    ),
 )
